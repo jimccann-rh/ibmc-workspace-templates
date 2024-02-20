b9ba506ed615dff1d425e545e4cafeafca3c6003
jimb0 <jimccann@redhat.com>
2024-02-20 13:35:36 -0500

    1st

--- prep.sh
+++ prep.sh
@@ -3,6 +3,9 @@ pip install ansible
 export PATH=$PATH:~/.local/bin
 echo "Put in IASS Classic API KEY"
 read apikey
+echo "Put in IASS Classic Username"
+read username
 export IAAS_CLASSIC_API_KEY=apikey
+export IAAS_CLASSIC_USERNAME=username
 ansible-playbook main.yml
 #ansible-playbook main.yml --extra-var manlookup=true
