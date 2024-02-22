430ccfcf2a260fc715f6e4784a81339d2f189c5b
jimb0 <jimccann@redhat.com>
2024-02-22 15:41:58 -0500

    update

---
+++ act-management.js
@@ -0,0 +1,23 @@
+{
+    "name": ">{{ act.name_actions_man_name }}",
+    "description": "Setup management server",
+    "location": "us-south",
+    "resource_group": "Default",
+    "bastion_connection_type": "ssh",
+    "inventory_connection_type": "winrm", 
+    "source": {
+        "source_type" : "git",
+        "git" : {
+            "git_repo_url": "{{ act.name_repo_man }}"
+        }
+    },
+    "command_parameter": "{{ act.name_playbook }}",
+    "bastion": {},
+    "bastion_credentials": {
+	    "metadata": {}
+    },
+    "tags":  ["{{ act.name_tags }}", "management"], 
+    "source_readme_url": "stringtype",
+    "source_type": "GitHub"
+}
+
