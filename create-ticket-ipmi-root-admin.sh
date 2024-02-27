f2642e7dae3e7e7e464b57f1a78714604d12af73
jimb0 <jimccann@redhat.com>
2024-02-26 21:41:36 -0500

    create ticket to get correct access for ipmi

--- create-ticket-ipmi-root-admin.sh
+++ create-ticket-ipmi-root-admin.sh
@@ -9,7 +9,7 @@ hardware=$(jq '.[].id' data.json)
 
 for id in $hardware; do
   echo "ID: $id creating ticket"
-  ibmcloud sl ticket create --title "Update root account access in IPMI" --body "Please update the Remote management IPMI to role of Administrator if it is set to Operator. I can not run Redfish commands" --subject-id 1021 --attachment $id --attachment-type hardware --rootpwd passw0rd
+  ibmcloud sl ticket create --title "Update root account access in IPMI" --body "Please update the Remote management IPMI to role of Administrator if it is set to Operator. I can not run Redfish commands." --subject-id 1021 --attachment $id --attachment-type hardware --rootpwd passw0rd
 done
 
 
